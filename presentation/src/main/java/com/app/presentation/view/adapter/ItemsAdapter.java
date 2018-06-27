a52123ce6d4abceb88424dea6bcc8d67a648b26b
Rana Ahsan Ali <ranaahsanali@gmail.com>
2018-06-27 14:27:35 +0400

    Update project to latest android changes

--- presentation/src/main/java/com/app/presentation/view/adapter/ItemsAdapter.java
+++ presentation/src/main/java/com/app/presentation/view/adapter/ItemsAdapter.java
@@ -8,92 +8,106 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import butterknife.Bind;
-import butterknife.ButterKnife;
+
 import com.app.presentation.R;
 import com.app.presentation.component.ImageHelper;
 import com.app.presentation.model.ItemDetailModel;
+
 import java.util.Collections;
 import java.util.List;
+
 import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Adaptar that manages item collection of {@link ItemDetailModel }.
  */
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
 
-  public interface OnItemClickListener {
-    void onItemClicked();
-  }
+    public interface OnItemClickListener {
+        void onItemClicked();
+    }
 
-  private List<ItemDetailModel> itemDetailModels;
-  private final LayoutInflater layoutInflater;
+    private List<ItemDetailModel> itemDetailModels;
+    private final LayoutInflater layoutInflater;
 
-  private OnItemClickListener onItemClickListener;
+    private OnItemClickListener onItemClickListener;
 
-  @Inject ItemsAdapter(Context context) {
-    this.layoutInflater =
-        (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    this.itemDetailModels = Collections.emptyList();
-  }
+    @Inject
+    ItemsAdapter(Context context) {
+        this.layoutInflater =
+                (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.itemDetailModels = Collections.emptyList();
+    }
 
-  @Override public int getItemCount() {
-    return (this.itemDetailModels != null) ? this.itemDetailModels.size() : 0;
-  }
+    @Override
+    public int getItemCount() {
+        return (this.itemDetailModels != null) ? this.itemDetailModels.size() : 0;
+    }
 
-  @Override public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    final View view = this.layoutInflater.inflate(R.layout.row_item, parent, false);
-    return new ItemViewHolder(view);
-  }
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View view = this.layoutInflater.inflate(R.layout.row_item, parent, false);
+        return new ItemViewHolder(view);
+    }
 
-  @Override public void onBindViewHolder(ItemViewHolder holder, final int position) {
-    final ItemDetailModel model = this.itemDetailModels.get(position);
-    holder.tvTitle.setText(model.getTitle());
-    holder.tvDes.setText(Html.fromHtml(model.getDescription()));
-    holder.tvPrice.setText(model.getPrice().toString());
-    ImageHelper.setImage(holder.view.getContext(), model.getImageUrl(), holder.ivImage);
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, final int position) {
+        final ItemDetailModel model = this.itemDetailModels.get(position);
+        holder.tvTitle.setText(model.getTitle());
+        holder.tvDes.setText(Html.fromHtml(model.getDescription()));
+        holder.tvPrice.setText(model.getPrice().toString());
+        ImageHelper.setImage(holder.view.getContext(), model.getImageUrl(), holder.ivImage);
 
-    holder.itemView.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        if (ItemsAdapter.this.onItemClickListener != null) {
-          ItemsAdapter.this.onItemClickListener.onItemClicked();
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ItemsAdapter.this.onItemClickListener != null) {
+                    ItemsAdapter.this.onItemClickListener.onItemClicked();
+                }
+            }
+        });
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public void setSearchResults(List<ItemDetailModel> itemDetailModels) {
+        this.validateCollection(itemDetailModels);
+        this.itemDetailModels = itemDetailModels;
+        this.notifyDataSetChanged();
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    private void validateCollection(List<ItemDetailModel> itemDetailModels) {
+        if (itemDetailModels == null) {
+            throw new IllegalArgumentException("The list cannot be null");
         }
-      }
-    });
-  }
-
-  @Override public long getItemId(int position) {
-    return position;
-  }
-
-  public void setSearchResults(List<ItemDetailModel> itemDetailModels) {
-    this.validateCollection(itemDetailModels);
-    this.itemDetailModels = itemDetailModels;
-    this.notifyDataSetChanged();
-  }
-
-  public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
-    this.onItemClickListener = onItemClickListener;
-  }
-
-  private void validateCollection(List<ItemDetailModel> itemDetailModels) {
-    if (itemDetailModels == null) {
-      throw new IllegalArgumentException("The list cannot be null");
     }
-  }
 
-  static class ItemViewHolder extends RecyclerView.ViewHolder {
-    View view;
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+        View view;
 
-    @Bind(R.id.title) TextView tvTitle;
-    @Bind(R.id.des) TextView tvDes;
-    @Bind(R.id.price) TextView tvPrice;
-    @Bind(R.id.image) ImageView ivImage;
+        @BindView(R.id.title)
+        TextView tvTitle;
+        @BindView(R.id.des)
+        TextView tvDes;
+        @BindView(R.id.price)
+        TextView tvPrice;
+        @BindView(R.id.image)
+        ImageView ivImage;
 
-    ItemViewHolder(View itemView) {
-      super(itemView);
-      this.view = itemView;
-      ButterKnife.bind(this, itemView);
+        ItemViewHolder(View itemView) {
+            super(itemView);
+            this.view = itemView;
+            ButterKnife.bind(this, itemView);
+        }
     }
-  }
 }
--- presentation/src/main/java/com/app/presentation/view/custom/QuantityView.java
+++ presentation/src/main/java/com/app/presentation/view/custom/QuantityView.java
@@ -3,80 +3,88 @@ package com.app.presentation.view.custom;
 import android.annotation.SuppressLint;
 import android.view.View;
 import android.widget.TextView;
-import butterknife.Bind;
-import butterknife.ButterKnife;
+
 import com.app.presentation.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by RanaAli on 8/21/17.
  */
 
 public class QuantityView {
-  private View view;
+    private View view;
 
-  protected @Bind(R.id.tv_quantity_minus) TextView tvQuantityMinus;
-  protected @Bind(R.id.tv_quantity) TextView tvQuantity;
-  protected @Bind(R.id.tv_quantity_plus) TextView tvQuantityPlus;
+    protected @BindView(R.id.tv_quantity_minus)
+    TextView tvQuantityMinus;
+    protected @BindView(R.id.tv_quantity)
+    TextView tvQuantity;
+    protected @BindView(R.id.tv_quantity_plus)
+    TextView tvQuantityPlus;
 
-  private Integer count = 1;
-  private Integer min = 1;
-  private Integer max = 10;
+    private Integer count = 1;
+    private Integer min = 1;
+    private Integer max = 10;
 
-  private QuantityListener quantityListener;
+    private QuantityListener quantityListener;
 
-  public QuantityView(View view) {
-    this.view = view;
-    ButterKnife.bind(this, this.view);
+    public QuantityView(View view) {
+        this.view = view;
+        ButterKnife.bind(this, this.view);
 
-    setup();
-  }
+        setup();
+    }
 
-  private void setup() {
-    setQuantity();
-
-    tvQuantityMinus.setOnClickListener(minusOnClickListener);
-    tvQuantityPlus.setOnClickListener(plusOnClickListener);
-  }
-
-  @SuppressLint("SetTextI18n") private void setQuantity() {
-    tvQuantity.setText(count.toString());
-  }
-
-  private View.OnClickListener plusOnClickListener = new View.OnClickListener() {
-    @Override public void onClick(View v) {
-      if (count < max) {
-        count++;
+    private void setup() {
         setQuantity();
-        triggerCallback();
-      }
+
+        tvQuantityMinus.setOnClickListener(minusOnClickListener);
+        tvQuantityPlus.setOnClickListener(plusOnClickListener);
     }
-  };
 
-  private View.OnClickListener minusOnClickListener = new View.OnClickListener() {
-    @Override public void onClick(View v) {
-      if (count > min) {
-        count--;
-        setQuantity();
-        triggerCallback();
-      }
+    @SuppressLint("SetTextI18n")
+    private void setQuantity() {
+        tvQuantity.setText(count.toString());
     }
-  };
 
-  private void triggerCallback() {
-    if (quantityListener != null) {
-      quantityListener.onQuantityChanged(count);
+    private View.OnClickListener plusOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (count < max) {
+                count++;
+                setQuantity();
+                triggerCallback();
+            }
+        }
+    };
+
+    private View.OnClickListener minusOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (count > min) {
+                count--;
+                setQuantity();
+                triggerCallback();
+            }
+        }
+    };
+
+    private void triggerCallback() {
+        if (quantityListener != null) {
+            quantityListener.onQuantityChanged(count);
+        }
     }
-  }
 
-  public void setMax(Integer max) {
-    this.max = max;
-  }
+    public void setMax(Integer max) {
+        this.max = max;
+    }
 
-  public void setQuantityListener(QuantityListener quantityListener) {
-    this.quantityListener = quantityListener;
-  }
+    public void setQuantityListener(QuantityListener quantityListener) {
+        this.quantityListener = quantityListener;
+    }
 
-  public interface QuantityListener {
-    void onQuantityChanged(int quantity);
-  }
+    public interface QuantityListener {
+        void onQuantityChanged(int quantity);
+    }
 }
